7fc13580641c71390d457e491dcb264a81e2db37
Jean-Paul Calderone <exarkun@divmod.com>
2007-05-05 15:09:24 -0400

    movement observation

--- game/player.py
+++ game/player.py
@@ -5,10 +5,15 @@ class Player(object):
     An object with a position.
 
     @ivar position: A tuple of (x,y), specifying position of player.
+
+    @ivar observers: A list of objects which will be notified about state
+    changes on this instance.
     """
 
     def __init__(self, position):
         self.position = position
+        self.observers = []
+
 
     def move(self, offset):
         """
@@ -18,3 +23,12 @@ class Player(object):
         """
         posx, posy = self.position
         self.position = (posx + offset[0], posy + offset[1])
+        for observer in self.observers:
+            observer.moved(self, (posx, posy))
+
+
+    def addObserver(self, observer):
+        """
+        Notify the given object when this player's position changes.
+        """
+        self.observers.append(observer)
--- game/test/test_player.py
+++ game/test/test_player.py
@@ -17,7 +17,7 @@ class PlayerTests(unittest.TestCase):
         player = Player((0, 0))
         self.assertEqual(player.position, (0, 0))
 
-    
+
     def test_move(self):
         """
         L{Player.move} should accept an vector which will be applied
@@ -26,3 +26,24 @@ class PlayerTests(unittest.TestCase):
         player = Player((2, 3))
         player.move((1, -2))
         self.assertEqual(player.position, (3, 1))
+
+
+    def test_movementObservation(self):
+        """
+        L{Player.move} should notify an observer that the movement occurred.
+        """
+        startPosition = (3, 2)
+        player = Player(startPosition)
+
+        class MovementObserver(object):
+            def __init__(self):
+                self.movements = []
+
+            def moved(self, what, previousPosition):
+                self.movements.append((what, previousPosition, what.position))
+        observer = MovementObserver()
+
+        # XXX Should this be addMovementObserver?
+        player.addObserver(observer)
+        player.move((-2, 1))
+        self.assertEqual(observer.movements, [(player, startPosition, (1, 3))])
