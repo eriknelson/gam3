76818f464f87bf58e40061287ea888e84ef33560
Jean-Paul Calderone <exarkun@divmod.com>
2010-10-23 08:34:42 -0400

    Functional test for terrain rendering

--- game/functional/test_view.py
+++ game/functional/test_view.py
@@ -9,10 +9,24 @@ from game.player import Player
 
 from game.controller import PlayerController
 
-from game.view import Window
-from game.view import PlayerView
+from game.view import Window, loadImage
+from game.view import PlayerView, TerrainView
+from game.terrain import GRASS
 
-class PlayerTests(TestCase):
+
+class FunctionalTestMixin:
+    def setUp(self):
+        # XXX PRIVATE VARIABLE USAGE ZOMG
+        print getattr(self, self._testMethodName).__doc__
+
+
+    def tearDown(self):
+        if not raw_input("Did it work?").lower().startswith('y'):
+            self.fail("User specified test failure")
+
+
+
+class PlayerTests(FunctionalTestMixin, TestCase):
     def setUp(self):
         self.environment = Environment(50, reactor)
         self.environment.start()
@@ -21,14 +35,13 @@ class PlayerTests(TestCase):
             speed=75,
             seconds=self.environment.seconds)
 
-        # XXX PRIVATE VARIABLE USAGE ZOMG
-        print getattr(self, self._testMethodName).__doc__
+        FunctionalTestMixin.setUp(self)
 
 
     def tearDown(self):
         self.environment.stop() # clean up pending calls
-        if not raw_input("Did it work?").lower().startswith('y'):
-            self.fail("User specified test failure")
+        FunctionalTestMixin.tearDown(self)
+
 
     def test_tedium(self):
         """
@@ -71,4 +84,21 @@ class PlayerTests(TestCase):
         view = PlayerView(self.player)
         window.add(view)
         window.submitTo(PlayerController(self.player))
+        return window.go()
+
+
+
+class TerrainTests(FunctionalTestMixin, TestCase):
+    """
+    Tests for how terrain is rendered in the view.
+    """
+    def test_singleTile(self):
+        """
+        There should be a single square of grass terrain in the bottom left
+        corner.
+        """
+        environment = Environment(50, reactor)
+        window = Window(environment)
+        view = TerrainView({(0, 0): GRASS}, loadImage)
+        window.add(view)
         return window.go()
