c9dc0453358af06916e9eec73350c2760f81de87
Jean-Paul Calderone <exarkun@divmod.com>
2010-11-02 21:05:48 -0400

    Update test for 3d coordinate space

--- game/test/test_player.py
+++ game/test/test_player.py
@@ -130,10 +130,10 @@ class PlayerTests(unittest.TestCase, PlayerCreationMixin):
         L{Player.getPosition} will returns C{float} values if the player's
         coordinates don't fall exactly onto integer values.
         """
-        player = self.makePlayer((0, 0))
+        player = self.makePlayer(Vertex(0, 0, 0))
         player.setDirection(FORWARD)
         self.advanceTime(0.5)
-        self.assertEquals(player.getPosition(), (0, 0.5))
+        self.assertEquals(player.getPosition(), Vertex(0, 0, -0.5))
 
 
     def test_stop(self):
