8ba95c3aeeb2d85aa064dde277bcd0f707ff28f8
Christopher Armstrong <radix@twistedmatrix.com>
2013-03-05 16:01:00 -0600

    copyright information. relicense gam3 to MIT.

--- gam3/__init__.py
+++ gam3/__init__.py
@@ -1,16 +1,6 @@
 # -*- test-case-name: gam3 -*-
-# Copyright (C) Christopher J. Armstrong 2007 ALL RIGHTS RESERVED
-# Copyright (C) Jean-Paul Calderone 2007 ALL RIGHTS RESERVED
+# Copyright (C) the contributors. See LICENSE for details.
 
 """
-Copyright (C) Christopher J. Armstrong 2007 ALL RIGHTS RESERVED
-Copyright (C) Jean-Paul Calderone 2007 ALL RIGHTS RESERVED
-
 Server implementation for the Game system.
-
-Copyright (C) Christopher J. Armstrong 2007 ALL RIGHTS RESERVED
-Copyright (C) Jean-Paul Calderone 2007 ALL RIGHTS RESERVED
 """
-
-# Copyright (C) Christopher J. Armstrong 2007 ALL RIGHTS RESERVED
-# Copyright (C) Jean-Paul Calderone 2007 ALL RIGHTS RESERVED
